f467a1477ecb181bf723aee77cd3bdbe1a282b40
刘放 <362512489@qq.com>
2019-04-03 09:49:20 +0800

    init version

--- bin/mrg.js
+++ bin/mrg.js
@@ -2,23 +2,10 @@
 
 // Check node version before requiring/doing anything else
 // The user may be on a very old node version
-const findup = require('findup-sync');
 
-const versionUtil = require("../lib/util/version");
-const Config = require("../lib/util/config");
-const MrgLoop = require("../lib/loop");
 const registCommander = require('../lib/commander')
-//judge environment factors
-versionUtil.judgeEnv("mrg");
+const curVersion = require("../package.json").version;
 
-let config = new Config();
-config.getProjectList().then(projectList => {
-  let mrgLoop = new MrgLoop({ lists: projectList });
-  // mrgLoop.loopCommand('git pull');
-});
-
-
-const pkg = require(findup('package.json', { cwd: __dirname }));
 
 // 注册命令
-registCommander(pkg.version);
+registCommander(curVersion);
